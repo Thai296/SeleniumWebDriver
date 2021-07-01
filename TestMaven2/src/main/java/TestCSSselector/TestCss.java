aedfdd29108ec360560b5e694649491deec86cdc
TraOLong <TraOLong@TraOLong>
2021-07-01 00:07:09 -0400

    commit

--- TestMaven2/src/main/java/TestCSSselector/TestCss.java
+++ TestMaven2/src/main/java/TestCSSselector/TestCss.java
@@ -9,12 +9,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 public class TestCss {
 	WebDriver driver;
